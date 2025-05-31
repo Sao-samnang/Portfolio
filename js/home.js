88740ad448145eb22dc04c83b4c9eb65b9a432a5
Sao-samnang <saosamnang022@gmail.com>
2025-05-31 09:52:54 +0700

    commit v--0.0.1

--- js/home.js
+++ js/home.js
@@ -5,8 +5,4 @@ $(document).ready(function(){
         $(".contain-body").css("display","block")
         $("body").css("height","auto")
     },1000);
-    $('.btn-home-top .bhire').click(function(){
-        window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=sharing")
-    })
-    
 })
